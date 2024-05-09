b7640b5dca9c72d2109e54c0c2ed04b673a5485b
DESKTOP-PR2BK7P\csi23-mserina <csi23-mserina@colegioaltair.net>
2024-05-09 10:46:46 +0200

    AÑADIDO[ ] VERIFICAR CAMPO TELEFONO Y PRINCIPIO DEL CARRTITO

--- edu.MSMProyectoBootstrap/src/main/webapp/js.js
+++ edu.MSMProyectoBootstrap/src/main/webapp/js.js
@@ -2,20 +2,44 @@
  * 
  */
 
+let veces = 0;
+const dataStored = localStorage.getItem("estado");
+if (dataStored) {
+   veces = dataStored;
+}
+if (document.getElementById("contador"))
+   document.getElementById("contador").innerHTML = veces
+
+let arr =[0,0]
+const dataStored2 = localStorage.getItem("array");
+if (dataStored2) {
+   //arr = dataStored2;
+   arr = JSON.parse(dataStored2)
+}
+if (document.getElementById("contador2"))
+   document.getElementById("contador2").innerHTML = typeof(arr)
+
+
+
 
 
 
 function validadorCampos(){
 	
-	hola();
-	apellidos();
-	gmail();
+	
+	if((hola() == true) && (apellidos() == true) && (gmail() == true) && (telefono() == true))
+	{
+		return true;	
+	}
+	else{
+		return false;
+	}
 }
 
 function hola(){
 	alert("hola")
-	const nombre = document.getElementById('nombre');
-	const errorNombre = document.getElementById('error-nombre');
+	let nombre = document.getElementById('nombre');
+	let errorNombre = document.getElementById('error-nombre');
     var valor = nombre.value;
     let resultado = "";
 
@@ -35,9 +59,9 @@ function hola(){
 }
 
 function apellidos(){
-	alert("hola")
-	const apellido = document.getElementById('apellidos');
-	const errorApellidos = document.getElementById('error-apellidos');
+	alert("apellidos")
+	let apellido = document.getElementById('apellidos');
+	let errorApellidos = document.getElementById('error-apellidos');
     var valor = apellido.value;
     let resultado = "";
 
@@ -57,9 +81,9 @@ function apellidos(){
 }
 
 function gmail(){
-	alert("hola")
-	const gmail = document.getElementById('gmail');
-	const errorGmail = document.getElementById('error-gmail');
+	alert("gmail")
+	let gmail = document.getElementById('gmail');
+	let errorGmail = document.getElementById('error-gmail');
     var valor = gmail.value;
     let resultado = "";
 
@@ -79,6 +103,27 @@ function gmail(){
 }
 
 
+function telefono(){
+	alert("telefono")
+	let telefono = document.getElementById('telefono');
+	let errorTelefono = document.getElementById('error-telefono');
+    var valor = telefono.value;
+    let resultado = "";
+
+    // Validar si el nombre es un número
+    if(isNaN(valor) || valor.length > 9){
+        resultado = false;
+        errorTelefono.style.display = 'block';
+        errorTelefono.style.borderColor = 'red';
+        return false;
+    } else {
+        resultado = true;
+        errorTelefono.style.display = 'none';
+        errorTelefono.style.borderColor = ''; 
+        alert("Introdujo bien el telefono");
+        return true;
+    }
+}
 
 
 
